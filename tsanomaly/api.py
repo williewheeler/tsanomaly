397177bbaab2a92bb49f45f89cfa1efe29bf9fd9
Willie Wheeler <willie.wheeler@gmail.com>
2022-03-07 00:51:36 -0800

    EWMA and PEWMA code streamlining

--- tsanomaly/api.py
+++ tsanomaly/api.py
@@ -4,6 +4,19 @@ import pandas as pd
 from math import *
 
 
+def _to_result(x, mu, sigma, m):
+    upper = mu + m * sigma
+    lower = mu - m * sigma
+    anom = (x < lower) | (x > upper)
+    return pd.DataFrame({
+        "x" : x,
+        "mean" : mu,
+        "upper" : upper,
+        "lower" : lower,
+        "anomaly" : anom,
+    })
+
+
 def ewma(x, alpha, m, T=5):
     """
     Anomaly detection based on the exponentially-weighted moving average.
@@ -21,13 +34,7 @@ def ewma(x, alpha, m, T=5):
     """
 
     ewm = x.shift().ewm(alpha=alpha, min_periods=T)
-    mu = ewm.mean()
-    sigma = ewm.std()
-    upper = mu + m * sigma
-    lower = mu - m * sigma
-    anom = (x <= lower) | (x >= upper)
-    return pd.concat([x, mu, upper, lower, anom], axis=1) \
-        .set_axis(["x", "mean", "upper", "lower", "anomaly"], axis=1)
+    return _to_result(x, ewm.mean(), ewm.std(), m)
 
 
 def pewma(x, alpha, beta, m, T=5):
@@ -37,7 +44,8 @@ def pewma(x, alpha, beta, m, T=5):
     Parameters
     ----------
     X : data
-    alpha : current weight in [0.0, 1.0]
+    alpha : current weight in [0.0, 1.0]. Note that this is the reverse of what's in the paper
+            (i.e., they use alpha for the history weight)
     beta : probability weight, applied to alpha
     m : band multiplier
     T : training period
@@ -48,48 +56,29 @@ def pewma(x, alpha, beta, m, T=5):
     """
     PR_DENOM = sqrt(2.0 * pi)
     
-    # Flip alpha to match the paper.
-    # So now alpha is the history weight in [0.0, 1.0].
-    alpha = 1.0 - alpha
-
-    # Init
     n = len(x)
-    mu = np.repeat(x[0], n)
-    upper = np.repeat(x[0], n)
-    lower = np.repeat(x[0], n)
-    anom = np.repeat(False, n)    
     s1 = x[0]
     s2 = x[0] * x[0]
-    sigma = 1.0
-        
+    mu = np.repeat(x[0], n)
+    sigma = np.repeat(0.0, n)
+    
     for t in range(1, n):
-        
-        # Probabilistic alpha adjustment, during and after training period
         if t < T:
-            alpha_t = 1.0 - 1.0 / t
+            alpha_t = 1.0 / t
+        else if sigma[t-1] == 0:
+            alpha_t = alpha
         else:
             # Use z-score to calculate probability
-            z = (x[t-1] - mu[t-1]) / sigma
-            pr = np.exp(-0.5 * z * z) / PR_DENOM
-            alpha_t = (1.0 - beta * pr) * alpha
+            z = (x[t-1] - mu[t-1]) / sigma[t-1]
+            p = np.exp(-0.5 * z * z) / PR_DENOM
+            alpha_t = (beta * p) + (1.0 + beta * p) * alpha
         
-        s1 = alpha_t * s1 + (1.0 - alpha_t) * x[t-1]
-        s2 = alpha_t * s2 + (1.0 - alpha_t) * x[t-1] * x[t-1]        
+        s1 = alpha_t * x[t-1] + (1.0 - alpha_t) * s1
+        s2 = alpha_t * x[t-1] * x[t-1] + (1.0 - alpha_t) * s2
         mu[t] = s1
-        sigma = sqrt(s2 - s1 * s1)
-        upper[t] = mu[t] + m * sigma
-        lower[t] = mu[t] - m * sigma
+        sigma[t] = sqrt(s2 - s1 * s1)
         
-        if t > T:
-            anom[t] = (x[t] > upper[t] or x[t] < lower[t])
-            
-    return pd.DataFrame({
-        "x" : x,
-        "mean" : mu,
-        "upper" : upper,
-        "lower" : lower,
-        "anomaly" : anom,
-    })
+    return _to_result(x, mu, sigma, m)
 
 
 def plot_anomalies(ax, df, title, show_mean=True, band_color="k", band_alpha=0.2):
