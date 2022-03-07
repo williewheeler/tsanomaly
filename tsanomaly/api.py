c796a6d4bdd47908b4343dcb35bc2b8741ba2e76
Willie Wheeler <willie.wheeler@gmail.com>
2022-03-06 18:48:40 -0800

    Updated EWMA implementation to use built-in pandas capabilities

--- tsanomaly/api.py
+++ tsanomaly/api.py
@@ -3,6 +3,7 @@ import pandas as pd
 
 from math import *
 
+
 def ewma(x, alpha, m, T=5):
     """
     Anomaly detection based on the exponentially-weighted moving average.
@@ -10,40 +11,23 @@ def ewma(x, alpha, m, T=5):
     Parameters
     ----------
     x : a pandas series
-    alpha : current vs history param [0.0, 1.0]
+    alpha : current weight in (0.0, 1.0]
     m : band multiplier
+    T : training period
     
     Returns
     -------
     Pandas DataFrame containing EWMA anomaly results
     """
-    
-    # Init
-    n = len(x)    
-    xhat = np.repeat(x[0], n)
-    upper = np.repeat(x[0], n)
-    lower = np.repeat(x[0], n)
-    anom = np.repeat(False, n)    
-    s1 = x[0]
-    s2 = x[0] * x[0]
-    
-    for t in range(1, n):
-        s1 = alpha * s1 + (1.0 - alpha) * x[t-1]
-        s2 = alpha * s2 + (1.0 - alpha) * x[t-1] * x[t-1]
-        xhat[t] = s1
-        sigma_hat = sqrt(s2 - s1 * s1)
-        upper[t] = xhat[t] + m * sigma_hat
-        lower[t] = xhat[t] - m * sigma_hat
-        if t > T:
-            anom[t] = (x[t] > upper[t] or x[t] < lower[t])
-            
-    return pd.concat([
-        pd.Series(x, name="x"),
-        pd.Series(xhat, name="mean", index=x.index),
-        pd.Series(upper, name="upper", index=x.index),
-        pd.Series(lower, name="lower", index=x.index),
-        pd.Series(anom, name="anomaly", index=x.index),
-    ], axis=1)
+
+    ewm = x.shift().ewm(alpha=alpha, min_periods=T)
+    mu = ewm.mean()
+    sigma = ewm.std()
+    upper = mu + m * sigma
+    lower = mu - m * sigma
+    anom = (x <= lower) | (x >= upper)
+    return pd.concat([x, mu, upper, lower, anom], axis=1) \
+        .set_axis(["x", "mean", "upper", "lower", "anomaly"], axis=1)
 
 
 def pewma(x, alpha, beta, m, T=5):
@@ -53,7 +37,7 @@ def pewma(x, alpha, beta, m, T=5):
     Parameters
     ----------
     X : data
-    alpha : maximal current vs history param [0.0, 1.0]
+    alpha : current weight in [0.0, 1.0]
     beta : probability weight, applied to alpha
     m : band multiplier
     T : training period
@@ -63,16 +47,20 @@ def pewma(x, alpha, beta, m, T=5):
     Pandas DataFrame containing PEWMA anomaly results
     """
     PR_DENOM = sqrt(2.0 * pi)
+    
+    # Flip alpha to match the paper.
+    # So now alpha is the history weight in [0.0, 1.0].
+    alpha = 1.0 - alpha
 
     # Init
     n = len(x)
-    xhat = np.repeat(x[0], n)
+    mu = np.repeat(x[0], n)
     upper = np.repeat(x[0], n)
     lower = np.repeat(x[0], n)
     anom = np.repeat(False, n)    
     s1 = x[0]
     s2 = x[0] * x[0]
-    sigma_hat = 1.0
+    sigma = 1.0
         
     for t in range(1, n):
         
@@ -81,27 +69,27 @@ def pewma(x, alpha, beta, m, T=5):
             alpha_t = 1.0 - 1.0 / t
         else:
             # Use z-score to calculate probability
-            z = (x[t-1] - xhat[t-1]) / sigma_hat
+            z = (x[t-1] - mu[t-1]) / sigma
             pr = np.exp(-0.5 * z * z) / PR_DENOM
             alpha_t = (1.0 - beta * pr) * alpha
         
         s1 = alpha_t * s1 + (1.0 - alpha_t) * x[t-1]
         s2 = alpha_t * s2 + (1.0 - alpha_t) * x[t-1] * x[t-1]        
-        xhat[t] = s1
-        sigma_hat = sqrt(s2 - s1 * s1)
-        upper[t] = xhat[t] + m * sigma_hat
-        lower[t] = xhat[t] - m * sigma_hat
+        mu[t] = s1
+        sigma = sqrt(s2 - s1 * s1)
+        upper[t] = mu[t] + m * sigma
+        lower[t] = mu[t] - m * sigma
         
         if t > T:
             anom[t] = (x[t] > upper[t] or x[t] < lower[t])
             
-    return pd.concat([
-        pd.Series(x, name="x"),
-        pd.Series(xhat, name="mean", index=x.index),
-        pd.Series(upper, name="upper", index=x.index),
-        pd.Series(lower, name="lower", index=x.index),
-        pd.Series(anom, name="anomaly", index=x.index),
-    ], axis=1)
+    return pd.DataFrame({
+        "x" : x,
+        "mean" : mu,
+        "upper" : upper,
+        "lower" : lower,
+        "anomaly" : anom,
+    })
 
 
 def plot_anomalies(ax, df, title, show_mean=True, band_color="k", band_alpha=0.2):
