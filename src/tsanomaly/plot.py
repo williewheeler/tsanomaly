182b1e0df32a38d79c1e27589b33d7338c3e7ede
Willie Wheeler <willie.wheeler@gmail.com>
2022-03-12 01:24:17 -0800

    Removed duplicate code

--- src/tsanomaly/plot.py
+++ src/tsanomaly/plot.py
@@ -1,10 +1,24 @@
-def plot_anomalies(ax, df, title, show_mean=True, band_color="k", band_alpha=0.2):
+def plot_anomalies(
+    ax,
+    df,
+    title=None,
+    show_x=True,
+    show_band=True,
+    x_color="k",
+    band_color="gray",
+    band_alpha=0.5):
+    
+    """Plots mean, bounds, observations and anomalies.
+    """
+    
     anom = df.loc[df["anomaly"], "x"]
-    ax.set_title(title)
-    ax.plot(df["upper"], color=band_color, alpha=band_alpha, linestyle="--")
-    ax.plot(df["lower"], color=band_color, alpha=band_alpha, linestyle="--")
-    if show_mean:
+    if title:
+        ax.set_title(title)
+    if show_band:
+        ax.plot(df["upper"], color=band_color, alpha=band_alpha, linestyle="--")
+        ax.plot(df["lower"], color=band_color, alpha=band_alpha, linestyle="--")
         ax.plot(df["mean"], color=band_color, alpha=band_alpha, linestyle="--")
-    ax.plot(df["x"], linestyle="-")
+    if show_x:
+        ax.plot(df["x"], linestyle="-")
     ax.scatter(x=anom.index, y=anom.values, color="r")
 
