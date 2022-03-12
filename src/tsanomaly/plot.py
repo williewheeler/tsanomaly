c5588649158887c209abd0c7d760b28fc4096ae5
Willie Wheeler <willie.wheeler@gmail.com>
2022-03-12 01:43:24 -0800

    Minor tweak

--- src/tsanomaly/plot.py
+++ src/tsanomaly/plot.py
@@ -4,6 +4,7 @@ def plot_anomalies(
     title=None,
     show_x=True,
     show_band=True,
+    show_anom=True,
     x_color="k",
     band_color="gray",
     band_alpha=0.5):
@@ -11,7 +12,6 @@ def plot_anomalies(
     """Plots mean, bounds, observations and anomalies.
     """
     
-    anom = df.loc[df["anomaly"], "x"]
     if title:
         ax.set_title(title)
     if show_band:
@@ -20,5 +20,6 @@ def plot_anomalies(
         ax.plot(df["mean"], color=band_color, alpha=band_alpha, linestyle="--")
     if show_x:
         ax.plot(df["x"], linestyle="-")
-    ax.scatter(x=anom.index, y=anom.values, color="r")
-
+    if show_anom:
+        anom = df.loc[df["anomaly"], "x"]
+        ax.scatter(x=anom.index, y=anom.values, color="r")
